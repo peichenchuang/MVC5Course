2d4c54abf92fff9fab8663eefc5773971b88c9d3
PeiChen Chuang <PeiChen.Chuang@mwhglobal.com>
2017-05-06 13:50:17 +0800

    套用 MetadataType 使用 Partial Class 擴充 Model 的 Attributei

--- MVC5Course/Models/Product.cs
+++ MVC5Course/Models/Product.cs
@@ -11,8 +11,7 @@ namespace MVC5Course.Models
 {
     using System;
     using System.Collections.Generic;
-    using System.ComponentModel.DataAnnotations;
-
+    
     public partial class Product
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -20,17 +19,11 @@ namespace MVC5Course.Models
         {
             this.OrderLine = new HashSet<OrderLine>();
         }
+    
         public int ProductId { get; set; }
-        [Required(ErrorMessage = "請輸入商品名稱")]
-        [MinLength(3, ErrorMessage ="商品長度不能低於3個字元")]
         public string ProductName { get; set; }
-        [Required]
-        [Range(0, 9999, ErrorMessage ="請輸入正確的商品價格")]
         public Nullable<decimal> Price { get; set; }
-        [Required]
         public Nullable<bool> Active { get; set; }
-        [Required]
-        [Range(0, 100, ErrorMessage = "請輸入正確數量")]
         public Nullable<decimal> Stock { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
