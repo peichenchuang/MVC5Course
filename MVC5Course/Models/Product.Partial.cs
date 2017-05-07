942603d0dbeb6e328e2f262d8041282eef780b95
PeiChen Chuang <PeiChen.Chuang@mwhglobal.com>
2017-05-07 13:40:10 +0800

    18 練習透過導覽屬性擴充原本 Product 資料模型的屬性並在 /EF 頁面加入此屬性內容(曾加一個計算型的欄位)

--- MVC5Course/Models/Product.Partial.cs
+++ MVC5Course/Models/Product.Partial.cs
@@ -8,6 +8,13 @@ namespace MVC5Course.Models
     [MetadataType(typeof(ProductMetaData))]
     public partial class Product
     {
+        [DisplayName("訂單數量")]
+        public int 訂單數量 {
+            get
+            {
+                return this.OrderLine.Count;
+            }
+        }
     }
     
     public partial class ProductMetaData
