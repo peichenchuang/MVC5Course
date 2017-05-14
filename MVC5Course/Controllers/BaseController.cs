419fcac6b69ce49e067fe6f742964ab2efa4bb2d
PeiChen Chuang <PeiChen.Chuang@mwhglobal.com>
2017-05-14 12:00:11 +0800

    42 練習透過 [HandleError] 自訂 DbEntityValidationException 例外錯誤頁面

--- MVC5Course/Controllers/BaseController.cs
+++ MVC5Course/Controllers/BaseController.cs
@@ -7,9 +7,11 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using MVC5Course.Models;
+using System.Data.Entity.Validation;
 
 namespace MVC5Course.Controllers
 {
+    [HandleError(ExceptionType =typeof(DbEntityValidationException), View = "Error_DbEntityValidationException")]
     public class BaseController : Controller
     {
         protected FabricsEntities db = new FabricsEntities();
