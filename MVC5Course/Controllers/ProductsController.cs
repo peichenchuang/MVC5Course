5a9b579bff3e7fc1e4dd5edb921beacb23ee17e6
PeiChen Chuang <PeiChen.Chuang@mwhglobal.com>
2017-05-14 11:39:59 +0800

    41 練習透過 [HandleError] 自訂各種例外錯誤頁面

--- MVC5Course/Controllers/ProductsController.cs
+++ MVC5Course/Controllers/ProductsController.cs
@@ -8,6 +8,8 @@ using System.Web;
 using System.Web.Mvc;
 using MVC5Course.Models;
 using MVC5Course.Models.ViewModels;
+using System.Data.SqlClient;
+using System.Data.Entity.Infrastructure;
 
 namespace MVC5Course.Controllers
 {
@@ -73,6 +75,7 @@ namespace MVC5Course.Controllers
         // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
         // 詳細資訊，請參閱 http://go.microsoft.com/fwlink/?LinkId=317598。
         [HttpPost]
+        [HandleError(ExceptionType =typeof(DbUpdateException), View = "Error_DbUpdateException")]
         [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "ProductId,ProductName,Price,Active,Stock")] Product product)
         {
