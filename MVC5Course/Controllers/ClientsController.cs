091edb61f10f3cc86f39fcf1e6f335c099e3ddff
PeiChen Chuang <PeiChen.Chuang@mwhglobal.com>
2017-05-21 15:38:53 +0800

    67 使用 PagedList.Mvc 實作分頁功能

--- MVC5Course/Controllers/ClientsController.cs
+++ MVC5Course/Controllers/ClientsController.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using MVC5Course.Models;
+using PagedList;
 
 namespace MVC5Course.Controllers
 {
@@ -15,10 +16,10 @@ namespace MVC5Course.Controllers
         private FabricsEntities db = new FabricsEntities();
 
         // GET: Clients
-        public ActionResult Index()
+        public ActionResult Index(int pageNo = 1)
         {
-            var client = db.Client.Include(c => c.Occupation).Take(10);
-            return View(client.ToList());
+            var client = db.Client.OrderBy(p=>p.ClientId).ToPagedList(pageNo, 10);
+            return View(client);
         }
 
         // GET: Clients/Details/5
