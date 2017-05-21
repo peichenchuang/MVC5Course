aaeb224700872e0e9b4615efe58d4f692acb04cb
PeiChen Chuang <PeiChen.Chuang@mwhglobal.com>
2017-05-21 10:06:26 +0800

    示範 DataType.Date 與 DisplayFormat 在搭配使用上的注意事項 日期格式一定要是 yyyy-MM-dd 格式

--- MVC5Course/Models/Product.Partial.cs
+++ MVC5Course/Models/Product.Partial.cs
@@ -11,7 +11,8 @@ namespace MVC5Course.Models
     public partial class Product : IValidatableObject
     {
         [DisplayName("訂單數量")]
-        public int 訂單數量 {
+        public int 訂單數量
+        {
             get
             {
                 return this.OrderLine.Count;
@@ -25,31 +26,31 @@ namespace MVC5Course.Models
         public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
         {
             //綜合運算 & 多重驗證
-            if(this.Price > 100 && this.Stock < 5)
+            if (this.Price > 100 && this.Stock < 5)
             {
-                yield return new ValidationResult("價格與庫存數量不合理", new string[] {"Price", "Stock" });
+                yield return new ValidationResult("價格與庫存數量不合理", new string[] { "Price", "Stock" });
             }
             if (this.OrderLine.Count() > 5 && this.Stock == 0)
             {
-                yield return new ValidationResult("Stock與訂單數量不合理", new string[] { "Stock"});
+                yield return new ValidationResult("Stock與訂單數量不合理", new string[] { "Stock" });
             }
             yield break;
         }
     }
-    
+
     public partial class ProductMetaData
     {
         [Required]
         public int ProductId { get; set; }
         [Required(ErrorMessage = "請輸入商品名稱")]
-        [商品名稱必須包含Will(ErrorMessage ="商品名稱需包含 will")]
+        [商品名稱必須包含Will(ErrorMessage = "商品名稱需包含 will")]
         //[Display(Name ="商品名稱")]
         [DisplayName("商品名稱")]
-        [StringLength(80, ErrorMessage="欄位長度不得大於 80 個字元")]
+        [StringLength(80, ErrorMessage = "欄位長度不得大於 80 個字元")]
         [MinLength(3, ErrorMessage = "商品長度不能低於3個字元")]
         public string ProductName { get; set; }
         [Required]
-        [Display(Name ="價格")]
+        [Display(Name = "價格")]
         [DisplayFormat(DataFormatString = "{0:0}", ApplyFormatInEditMode = true)]
         [Range(0, 9999, ErrorMessage = "請輸入正確的商品價格")]
         public Nullable<decimal> Price { get; set; }
@@ -57,9 +58,11 @@ namespace MVC5Course.Models
         [Display(Name = "有效")]
         public Nullable<bool> Active { get; set; }
         [Required]
-        [Display(Name ="數量")]
+        [Display(Name = "數量")]
         public Nullable<decimal> Stock { get; set; }
-        [Display(Name ="建立時間")]
+        [Display(Name = "建立時間")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public System.DateTime CreatedOn { get; set; }
         public virtual ICollection<OrderLine> OrderLine { get; set; }
     }
