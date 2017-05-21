f99301d464503c53d16108b483d277218af7853b
PeiChen Chuang <PeiChen.Chuang@mwhglobal.com>
2017-05-21 13:28:36 +0800

    63 自訂特定模型的顯示範本 (DisplayTemplates)

--- MVC5Course/Controllers/FormController.cs
+++ MVC5Course/Controllers/FormController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using MVC5Course.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -23,7 +25,7 @@ namespace MVC5Course.Controllers
             ViewBag.LastNameFilter = new SelectList(lastName);
 
             var data = db.Client.Where(p => p.LastName.Contains(LastNameFilter));
-            if(CreditRatingFilter != -1)
+            if (CreditRatingFilter != -1)
             {
                 data = data.Where(p => p.CreditRating == CreditRatingFilter);
             }
@@ -33,13 +35,14 @@ namespace MVC5Course.Controllers
 
         public ActionResult Edit(int id)
         {
+
             //這頁有 Model Binding，只有 id
             //ViewData.Model = db.Product.Find(id);
             //ViewData 只有一個強型別 Model 這樣就可以在 View 使用 model
             return View(db.Product.Find(id));
         }
         [HttpPost]
-        public ActionResult Action(int id, FormCollection form)
+        public ActionResult Edit(int id, FormCollection form)
         {
             var product = db.Product.Find(id);
 
@@ -51,6 +54,25 @@ namespace MVC5Course.Controllers
             }
 
             return View();
+        }
+
+        public ActionResult ClientEdit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Client client = db.Client.Find(id);
+            if (client == null)
+            {
+                return HttpNotFound();
+            }
+
+            var items = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            ViewBag.CreditRating = new SelectList(items);
+
+            ViewBag.OccupationId = new SelectList(db.Occupation, "OccupationId", "OccupationName", client.OccupationId);
+            return View(client);
         }
     }
 }
